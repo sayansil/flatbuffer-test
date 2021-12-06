a7fd6ded39bbb270a8fe3424bce2e80fc7f151fb
Sayan <sayan227sil@ymail.com>
2021-12-06 22:40:30 +0530

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,3 @@
+flatc --cpp -o output -I input --cpp-std c++17 input/master.fbs
+flatc --cpp -o output -I input --cpp-std c++17 input/plant.fbs
+flatc --cpp -o output -I input --cpp-std c++17 input/animal.fbs
