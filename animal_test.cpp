732379f26aace07e169de58a064064fa39342ad4
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2022-01-29 17:55:25 +0530

    Nested structs working efficiently

--- animal_test.cpp
+++ animal_test.cpp
@@ -13,13 +13,10 @@ int main()
     ram_builder.add_food_chain_rank(6);
     ram_builder.add_chromosome_number(4);
     
-    // create organism first
-    auto ram = Ecosystem::CreateOrganism(builder);
+    flatbuffers::Offset<Ecosystem::Organism> ram = ram_builder.Finish();
     
-    // finish building
-    ram_builder.Finish();
     builder.Finish(ram);
-    
+
     uint8_t * buffer_pointer = builder.GetBufferPointer();
     int size = builder.GetSize();
     for(int i = 0; i < size; i++)
@@ -27,7 +24,7 @@ int main()
     fmt::print("\nsize: {} bytes \n", size);
     
     // Call GetOrganism to get const pointer if no changes need to be made
-    auto ram_pointer = Ecosystem::GetMutableOrganism(buffer_pointer);
+    Ecosystem::Organism* ram_pointer = Ecosystem::GetMutableOrganism(buffer_pointer);
     
     fmt::print("chromosome_number: {} food_chain_rank: {}\n", ram_pointer->chromosome_number(), ram_pointer->food_chain_rank());
     ram_pointer->mutate_chromosome_number(9);
--- world_test.cpp
+++ world_test.cpp
@@ -6,25 +6,25 @@
 int main()
 {
     flatbuffers::FlatBufferBuilder builder;
-
-
-    // Creating a species object
-    auto species1 = Ecosystem::CreateSpecies(builder);
-    Ecosystem::SpeciesBuilder species_builder(builder);
-    species_builder.add_kind(builder.CreateString("deer"));
-    species_builder.Finish();
-
-    std::vector<flatbuffers::Offset<Ecosystem::Species>> species_vec;
-    species_vec.push_back(species1);
-
     Ecosystem::WorldBuilder world_builder(builder);
-    world_builder.add_species(builder.CreateVector(species_vec));
-    world_builder.add_year(builder.CreateString("2022"));
 
-    auto world = Ecosystem::CreateWorld(builder);
-    world_builder.Finish();
-    builder.Finish(world);
+    Ecosystem::SpeciesBuilder species_builder1(builder);
+    species_builder1.add_kind(builder.CreateString("animal"));
     
+    std::vector<flatbuffers::Offset<Ecosystem::Species>> stdvecSpecies;
+    stdvecSpecies.push_back(species_builder1.Finish());
+    
+    Ecosystem::SpeciesBuilder species_builder2(builder);
+    species_builder2.add_kind(builder.CreateString("plant"));
+    stdvecSpecies.push_back(species_builder2.Finish());
+
+    flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<Ecosystem::Species>>> species_vec = builder.CreateVector(stdvecSpecies.data(), stdvecSpecies.size());
+
+    world_builder.add_species(species_vec);
+    world_builder.add_year(builder.CreateString("2022"));
+    flatbuffers::Offset<Ecosystem::World> world_offset = world_builder.Finish();
+    builder.Finish(world_offset);
+
     uint8_t * buffer_pointer = builder.GetBufferPointer();
     int size = builder.GetSize();
     for(int i = 0; i < size; i++)
@@ -35,9 +35,14 @@ int main()
     fmt::print("Year: {}\n", world_pointer->year()->str());
     if(world_pointer->species())
     {
+        fmt::print("world has species\n");
         auto species_vec = world_pointer->species();
         int species_num = species_vec->size();
         fmt::print("Species num: {}\n", species_num);
+        auto species1 = species_vec->Get(0);
+        fmt::print("kind: {}\n", species1->kind()->str());
+        auto species2 = species_vec->Get(1);
+        fmt::print("kind: {}\n", species2->kind()->str());
     }
     else
     {
