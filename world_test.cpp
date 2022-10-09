6f0d52e698572291d19aabd337f6cd838f832af5
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2022-10-09 23:58:06 +0530

    Fixed incomplete creation of nested structures

--- world_test.cpp
+++ world_test.cpp
@@ -36,90 +36,87 @@ int8_t getValueAsByte(nlohmann::json &attributes, const std::string &key)
 
 flatbuffers::Offset<Ecosystem::Organism> createOrganism(
     flatbuffers::FlatBufferBuilder &builder,
-    Ecosystem::OrganismBuilder &organism_builder,
     nlohmann::json &attributes)
 {
-    organism_builder.add_kind(builder.CreateString(getValueAsStr(attributes, "kind").c_str()));
-    organism_builder.add_kingdom(builder.CreateString(getValueAsStr(attributes, "kingdom").c_str()));
-    organism_builder.add_chromosome_number(getValueAsUlong(attributes, "chromosome_number"));
-    organism_builder.add_food_chain_rank(getValueAsUlong(attributes, "food_chain_rank"));
-    organism_builder.add_is_asexual(Ecosystem::Reproduction(getValueAsByte(attributes, "is_asexual")));
-    organism_builder.add_age_fitness_on_death_ratio(getValueAsDouble(attributes, "age_fitness_on_death_ratio"));
-    organism_builder.add_conceiving_probability(getValueAsDouble(attributes, "conceiving_probability"));
-    organism_builder.add_mating_probability(getValueAsDouble(attributes, "mating_probability"));
-    organism_builder.add_mating_age_start(getValueAsDouble(attributes, "mating_age_start"));
-    organism_builder.add_mating_age_end(getValueAsDouble(attributes, "mating_age_end"));
-    organism_builder.add_max_age(getValueAsDouble(attributes, "max_age"));
-    organism_builder.add_mutation_probability(getValueAsDouble(attributes, "mutation_probability"));
-    organism_builder.add_offsprings_factor(getValueAsDouble(attributes, "offsprings_factor"));
-    organism_builder.add_height_on_speed(getValueAsDouble(attributes, "height_on_speed"));
-    organism_builder.add_height_on_stamina(getValueAsDouble(attributes, "height_on_stamina"));
-    organism_builder.add_height_on_vitality(getValueAsDouble(attributes, "height_on_vitality"));
-    organism_builder.add_weight_on_speed(getValueAsDouble(attributes, "weight_on_speed"));
-    organism_builder.add_weight_on_stamina(getValueAsDouble(attributes, "weight_on_stamina"));
-    organism_builder.add_weight_on_vitality(getValueAsDouble(attributes, "weight_on_vitality"));
-    organism_builder.add_vitality_on_appetite(getValueAsDouble(attributes, "vitality_on_appetite"));
-    organism_builder.add_vitality_on_speed(getValueAsDouble(attributes, "vitality_on_speed"));
-    organism_builder.add_stamina_on_appetite(getValueAsDouble(attributes, "stamina_on_appetite"));
-    organism_builder.add_stamina_on_speed(getValueAsDouble(attributes, "stamina_on_speed"));
-    organism_builder.add_theoretical_maximum_base_appetite(getValueAsDouble(attributes, "theoretical_maximum_base_appetite"));
-    organism_builder.add_theoretical_maximum_base_height(getValueAsDouble(attributes, "theoretical_maximum_base_height"));
-    organism_builder.add_theoretical_maximum_base_speed(getValueAsDouble(attributes, "theoretical_maximum_base_speed"));
-    organism_builder.add_theoretical_maximum_base_stamina(getValueAsDouble(attributes, "theoretical_maximum_base_stamina"));
-    organism_builder.add_theoretical_maheximum_base_vitality(getValueAsDouble(attributes, "theoretical_maheximum_base_vitality"));
-    organism_builder.add_theoretical_maximum_base_weight(getValueAsDouble(attributes, "theoretical_maximum_base_weight"));
-    organism_builder.add_theoretical_maximum_height(getValueAsDouble(attributes, "theoretical_maximum_height"));
-    organism_builder.add_theoretical_maximum_speed(getValueAsDouble(attributes, "theoretical_maximum_speed"));
-    organism_builder.add_theoretical_maximum_weight(getValueAsDouble(attributes, "theoretical_maximum_weight"));
-    organism_builder.add_theoretical_maximum_height_multiplier(getValueAsDouble(attributes, "theoretical_maximum_height_multiplier"));
-    organism_builder.add_theoretical_maximum_speed_multiplier(getValueAsDouble(attributes, "theoretical_maximum_speed_multiplier"));
-    organism_builder.add_theoretical_maximum_stamina_multiplier(getValueAsDouble(attributes, "theoretical_maximum_stamina_multiplier"));
-    organism_builder.add_theoretical_maximum_vitality_multiplier(getValueAsDouble(attributes, "theoretical_maximum_vitality_multiplier"));
-    organism_builder.add_theoretical_maximum_weight_multiplier(getValueAsDouble(attributes, "theoretical_maximum_weight_multiplier"));
-    organism_builder.add_name(builder.CreateString(getValueAsStr(attributes, "name").c_str()));
-    organism_builder.add_chromosome(builder.CreateString(getValueAsStr(attributes, "chromosome").c_str()));
-    organism_builder.add_gender(Ecosystem::Gender(getValueAsByte(attributes, "gender")));
-    organism_builder.add_generation(getValueAsUlong(attributes, "generation"));
-    organism_builder.add_immunity(getValueAsDouble(attributes, "immunity"));
-    organism_builder.add_base_appetite(getValueAsDouble(attributes, "base_appetite"));
-    organism_builder.add_base_height(getValueAsDouble(attributes, "base_height"));
-    organism_builder.add_base_speed(getValueAsDouble(attributes, "base_speed"));
-    organism_builder.add_base_stamina(getValueAsDouble(attributes, "base_stamina"));
-    organism_builder.add_base_vitality(getValueAsDouble(attributes, "base_vitality"));
-    organism_builder.add_base_weight(getValueAsDouble(attributes, "base_weight"));
-    organism_builder.add_height_multiplier(getValueAsDouble(attributes, "height_multiplier"));
-    organism_builder.add_speed_multiplier(getValueAsDouble(attributes, "speed_multiplier"));
-    organism_builder.add_stamina_multiplier(getValueAsDouble(attributes, "stamina_multiplier"));
-    organism_builder.add_vitality_multiplier(getValueAsDouble(attributes, "vitality_multiplier"));
-    organism_builder.add_weight_multiplier(getValueAsDouble(attributes, "weight_multiplier"));
-    organism_builder.add_max_height(getValueAsDouble(attributes, "max_height"));
-    organism_builder.add_max_weight(getValueAsDouble(attributes, "max_weight"));
-    organism_builder.add_age(getValueAsUlong(attributes, "age"));
-    organism_builder.add_height(getValueAsDouble(attributes, "height"));
-    organism_builder.add_weight(getValueAsDouble(attributes, "weight"));
-    organism_builder.add_age_death_factor(getValueAsDouble(attributes, "age_death_factor"));
-    organism_builder.add_fitness_death_factor(getValueAsDouble(attributes, "fitness_death_factor"));
-    organism_builder.add_death_factor(getValueAsDouble(attributes, "death_factor"));
-    organism_builder.add_static_fitness(getValueAsDouble(attributes, "static_fitness"));
-    organism_builder.add_max_appetite_at_age(getValueAsDouble(attributes, "max_appetite_at_age"));
-    organism_builder.add_max_speed_at_age(getValueAsDouble(attributes, "max_speed_at_age"));
-    organism_builder.add_max_stamina_at_age(getValueAsDouble(attributes, "max_stamina_at_age"));
-    organism_builder.add_max_vitality_at_age(getValueAsDouble(attributes, "max_vitality_at_age"));
-    organism_builder.add_vision_radius(getValueAsDouble(attributes, "vision_radius"));
-    organism_builder.add_sleep_restore_factor(getValueAsDouble(attributes, "sleep_restore_factor"));
-
-    return organism_builder.Finish();
+    return Ecosystem::CreateOrganism(builder,
+            builder.CreateString(getValueAsStr(attributes, "kind").c_str()),
+            builder.CreateString(getValueAsStr(attributes, "kingdom").c_str()),
+            getValueAsUlong(attributes, "chromosome_number"),
+            getValueAsUlong(attributes, "food_chain_rank"),
+            Ecosystem::Reproduction(getValueAsByte(attributes, "is_asexual")),
+            getValueAsDouble(attributes, "age_fitness_on_death_ratio"),
+            getValueAsDouble(attributes, "conceiving_probability"),
+            getValueAsDouble(attributes, "mating_probability"),
+            getValueAsDouble(attributes, "mating_age_start"),
+            getValueAsDouble(attributes, "mating_age_end"),
+            getValueAsDouble(attributes, "max_age"),
+            getValueAsDouble(attributes, "mutation_probability"),
+            getValueAsDouble(attributes, "offsprings_factor"),
+            getValueAsDouble(attributes, "height_on_speed"),
+            getValueAsDouble(attributes, "height_on_stamina"),
+            getValueAsDouble(attributes, "height_on_vitality"),
+            getValueAsDouble(attributes, "weight_on_speed"),
+            getValueAsDouble(attributes, "weight_on_stamina"),
+            getValueAsDouble(attributes, "weight_on_vitality"),
+            getValueAsDouble(attributes, "vitality_on_appetite"),
+            getValueAsDouble(attributes, "vitality_on_speed"),
+            getValueAsDouble(attributes, "stamina_on_appetite"),
+            getValueAsDouble(attributes, "stamina_on_speed"),
+            getValueAsDouble(attributes, "theoretical_maximum_base_appetite"),
+            getValueAsDouble(attributes, "theoretical_maximum_base_height"),
+            getValueAsDouble(attributes, "theoretical_maximum_base_speed"),
+            getValueAsDouble(attributes, "theoretical_maximum_base_stamina"),
+            getValueAsDouble(attributes, "theoretical_maheximum_base_vitality"),
+            getValueAsDouble(attributes, "theoretical_maximum_base_weight"),
+            getValueAsDouble(attributes, "theoretical_maximum_height"),
+            getValueAsDouble(attributes, "theoretical_maximum_speed"),
+            getValueAsDouble(attributes, "theoretical_maximum_weight"),
+            getValueAsDouble(attributes, "theoretical_maximum_height_multiplier"),
+            getValueAsDouble(attributes, "theoretical_maximum_speed_multiplier"),
+            getValueAsDouble(attributes, "theoretical_maximum_stamina_multiplier"),
+            getValueAsDouble(attributes, "theoretical_maximum_vitality_multiplier"),
+            getValueAsDouble(attributes, "theoretical_maximum_weight_multiplier"),
+            builder.CreateString(getValueAsStr(attributes, "name").c_str()),
+            builder.CreateString(getValueAsStr(attributes, "chromosome").c_str()),
+            Ecosystem::Gender(getValueAsByte(attributes, "gender")),
+            getValueAsUlong(attributes, "generation"),
+            getValueAsDouble(attributes, "immunity"),
+            getValueAsDouble(attributes, "base_appetite"),
+            getValueAsDouble(attributes, "base_height"),
+            getValueAsDouble(attributes, "base_speed"),
+            getValueAsDouble(attributes, "base_stamina"),
+            getValueAsDouble(attributes, "base_vitality"),
+            getValueAsDouble(attributes, "base_weight"),
+            getValueAsDouble(attributes, "height_multiplier"),
+            getValueAsDouble(attributes, "speed_multiplier"),
+            getValueAsDouble(attributes, "stamina_multiplier"),
+            getValueAsDouble(attributes, "vitality_multiplier"),
+            getValueAsDouble(attributes, "weight_multiplier"),
+            getValueAsDouble(attributes, "max_height"),
+            getValueAsDouble(attributes, "max_weight"),
+            getValueAsUlong(attributes, "age"),
+            getValueAsDouble(attributes, "height"),
+            getValueAsDouble(attributes, "weight"),
+            getValueAsDouble(attributes, "age_death_factor"),
+            getValueAsDouble(attributes, "fitness_death_factor"),
+            getValueAsDouble(attributes, "death_factor"),
+            getValueAsDouble(attributes, "static_fitness"),
+            getValueAsDouble(attributes, "max_appetite_at_age"),
+            getValueAsDouble(attributes, "max_speed_at_age"),
+            getValueAsDouble(attributes, "max_stamina_at_age"),
+            getValueAsDouble(attributes, "max_vitality_at_age"),
+            getValueAsDouble(attributes, "vision_radius"),
+            getValueAsDouble(attributes, "sleep_restore_factor")
+    );
 }
 
 flatbuffers::Offset<Ecosystem::Species> createSpecies(
     flatbuffers::FlatBufferBuilder &builder,
-    Ecosystem::SpeciesBuilder &species_builder,
     const std::string &kingdom,
     const std::string &kind,
     const int &num_organisms)
 {
     std::vector<flatbuffers::Offset<Ecosystem::Organism>> stdvecOrganisms;
-    Ecosystem::OrganismBuilder organism_builder(builder);
     nlohmann::json attributes;
 
     attributes["kind"] = kind;
@@ -128,31 +125,45 @@ flatbuffers::Offset<Ecosystem::Species> createSpecies(
     for (int i = 0; i < num_organisms; i++)
     {
         attributes["name"] = fmt::format("name_{}_{}", kind, i);
-        stdvecOrganisms.push_back(createOrganism(builder, organism_builder, attributes));
+        stdvecOrganisms.push_back(createOrganism(builder, attributes));
     }
 
-    species_builder.add_kingdom(builder.CreateString(kingdom.c_str()));
-    species_builder.add_kind(builder.CreateString(kind.c_str()));
-    species_builder.add_organism(builder.CreateVectorOfSortedTables(stdvecOrganisms.data(), stdvecOrganisms.size()));
-
-    return species_builder.Finish();
+    return Ecosystem::CreateSpecies(builder,
+                                    builder.CreateString(kind.c_str()),  /* kind */
+                                    builder.CreateString(kingdom.c_str()), /* kingdom */
+                                    builder.CreateVectorOfSortedTables(stdvecOrganisms.data(), stdvecOrganisms.size())
+                                    );
 }
 
 void createWorld(flatbuffers::FlatBufferBuilder &builder)
 {
-    Ecosystem::WorldBuilder world_builder(builder);
 
+    /*
+       While creating nested structures always CREATE everything first and only then ADD them to the buffer.
+       You cannot call any "create" function after any "add" function.
+       Inscribe these words on your heart and soul.
+       Piece of gold: https://github.com/google/flatbuffers/issues/276
+    */
+
+    // Create everything first
+
+    auto year = builder.CreateString("2022");
     std::vector<flatbuffers::Offset<Ecosystem::Species>> stdvecSpecies;
-    Ecosystem::SpeciesBuilder species_builder(builder);
 
-    stdvecSpecies.push_back(createSpecies(builder, species_builder, "animal", "deer", 2));
-    stdvecSpecies.push_back(createSpecies(builder, species_builder, "animal", "lion", 1));
-    stdvecSpecies.push_back(createSpecies(builder, species_builder, "plant", "bamboo", 3));
+    stdvecSpecies.push_back(createSpecies(builder, "animal", "deer", 2));
+    stdvecSpecies.push_back(createSpecies(builder, "animal", "lion", 1));
+    stdvecSpecies.push_back(createSpecies(builder, "plant", "bamboo", 3));
 
     flatbuffers::Offset<flatbuffers::Vector<flatbuffers::Offset<Ecosystem::Species>>> species_vec = builder.CreateVectorOfSortedTables(stdvecSpecies.data(), stdvecSpecies.size());
 
+    // Creation ends addition begins
+
+    Ecosystem::WorldBuilder world_builder(builder);
+
+    //Ecosystem::SpeciesBuilder species_builder(builder);
+
+    world_builder.add_year(year);
     world_builder.add_species(species_vec);
-    world_builder.add_year(builder.CreateString("2022"));
     builder.Finish(world_builder.Finish());
 }
 
